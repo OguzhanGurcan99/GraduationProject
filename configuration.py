225d4545a180824ecd7679da1e3d817937ffaf48
Oguzhan95275 <oguzhangurcan99@gmail.com>
2024-03-17 21:25:17 +0300

    guncelleme-1703

---
+++ configuration.py
@@ -0,0 +1,22 @@
+BUGDAY_SHP_FILE_PATH = "C:\\Users\\oguzh\\PycharmProjects\\graduationProject\\mt_data\\01_Train_Test_Shapefile\\00_Train\\bugday.shp"
+DOMATES_SHP_FILE_PATH = "C:\\Users\\oguzh\\PycharmProjects\\graduationProject\\mt_data\\01_Train_Test_Shapefile\\00_Train\\domates.shp"
+MISIR_SHP_FILE_PATH = "C:\\Users\\oguzh\\PycharmProjects\\graduationProject\\mt_data\\01_Train_Test_Shapefile\\00_Train\\misir.shp"
+MISIR2_SHP_FILE_PATH = "C:\\Users\\oguzh\\PycharmProjects\\graduationProject\\mt_data\\01_Train_Test_Shapefile\\00_Train\\misir2.shp"
+PAMUK_SHP_FILE_PATH = "C:\\Users\\oguzh\\PycharmProjects\\graduationProject\\mt_data\\01_Train_Test_Shapefile\\00_Train\\pamuk.shp"
+UZUM_SHP_FILE_PATH = "C:\\Users\\oguzh\\PycharmProjects\\graduationProject\\mt_data\\01_Train_Test_Shapefile\\00_Train\\uzum.shp"
+YONCA_SHP_FILE_PATH = "C:\\Users\\oguzh\\PycharmProjects\\graduationProject\\mt_data\\01_Train_Test_Shapefile\\00_Train\\yonca.shp"
+ZEYTIN_SHP_FILE_PATH = "C:\\Users\\oguzh\\PycharmProjects\\graduationProject\\mt_data\\01_Train_Test_Shapefile\\00_Train\\zeytin.shp"
+
+COMPOSITE_RGB_TIF_FILE_PATH = "C:\\Users\\oguzh\\PycharmProjects\\graduationProject\\mt_data\\RGB.tif"
+
+CROP_COLOR_CODES = {
+    "bugday" : (255,0,0),   #red
+    "domates" : (0,255,0),  #green
+    "misir" : (0,0,255),    #blue
+    "misir2" : (0,0,255),    #blue
+    "pamuk" : (128,0,128),  #purple
+    "uzum" : (128,128,0),   #olive
+    "yonca" : (0,255,255),  #aqua
+    "zeytin" : (255,127,80),#coral
+    "diger" : (0,0,0)       #black
+}
--- read_mt_data.py
+++ read_mt_data.py
@@ -1,9 +1,13 @@
 import geopandas as gpd
 import utm
+from PIL import Image
 from shapely.geometry import Point, Polygon
 import rasterio
 import numpy as np
 import cv2
+
+import configuration
+from configuration import *
 
 def isPointInsidePolygon(point, polygon):
     '''
@@ -29,46 +33,92 @@ def read_shp_file(path):
         total_list.append(converted_list)
     return total_list
 
-def cutPatchAndCreateMask(raster, xSize, ySize, startX, startY, polygons):
-    rasterMap = np.random.randint(255, size=(xSize, ySize))
-    rasterMask = np.random.randint(255, size=(xSize, ySize))
+def cutPatchAndCreateMask(raster, xSize, ySize, startX, startY, polygon_dict):
+    rasterMap = np.zeros((xSize, ySize, 3))
+    rasterMask = np.zeros((xSize, ySize, 3))
+    temp_poly=None
     for i in range(startX, startX+xSize):
         for j in range(startY, startY+ySize):
-            rasterMap[i-startX][j-startY]=raster.read(1)[i][j]
+            rasterMap[i-startX][j-startY][0]=raster.read(1)[i][j]
+            rasterMap[i-startX][j-startY][1]=raster.read(2)[i][j]
+            rasterMap[i-startX][j-startY][2]=raster.read(3)[i][j]
+
             val = raster.xy(i, j)
             lat, lon = utm.to_latlon(val[0], val[1], 35, northern=True)
             point = Point(lat, lon)
-            flag = True
-            for k in polygons:
-                poly = Polygon(k)
+            '''
+            if temp_poly:
                 result = isPointInsidePolygon(point, poly)
                 if result:
-                    flag = False
-                    rasterMask[i-startX][j-startY] = 255
+                    rasterMask[i-startX][j-startY][0] = CROP_COLOR_CODES[k][0]
+                    rasterMask[i-startX][j-startY][1] = CROP_COLOR_CODES[k][1]
+                    rasterMask[i-startX][j-startY][2] = CROP_COLOR_CODES[k][2]
+                else:
+                    temp_poly=None
+                continue
+            '''
+
+            for k, v in polygon_dict.items():
+                flag = True
+                for each_poly in v:
+                    poly = Polygon(each_poly)
+                    result = isPointInsidePolygon(point, poly)
+                    if result:
+                        temp_poly = poly
+                        flag = False
+                        color = CROP_COLOR_CODES[k]
+                        rasterMask[i-startX][j-startY][0] = color[0]
+                        rasterMask[i-startX][j-startY][1] = color[1]
+                        rasterMask[i-startX][j-startY][2] = color[2]
+                        break
+                if flag:
+                    rasterMask[i-startX][j-startY][0] = 0
+                    rasterMask[i-startX][j-startY][1] = 0
+                    rasterMask[i-startX][j-startY][2] = 0
+                else:
                     break
-            if flag:
-                rasterMask[i-startX][j-startY] = 0
 
-    cv2.imwrite("image" + str(xSize) + "-" + str(ySize) + "-" + str(startX) + "-" + str(startY) + ".png", rasterMap)
-    cv2.imwrite("mask" + str(xSize) + "-" + str(ySize) + "-" + str(startX) + "-" + str(startY) + ".png", rasterMask)
+    rasterMap = np.clip(rasterMap, 0, 255).astype('uint8')
+    image = Image.fromarray(rasterMap, 'RGB')
+    image.save("imageRGB" + str(xSize) + "-" + str(ySize) + "-" + str(startX) + "-" + str(startY) + ".png")
+
+    rasterMask = np.clip(rasterMask, 0, 255).astype('uint8')
+    image = Image.fromarray(rasterMask, 'RGB')
+    image.save("maskRGB" + str(xSize) + "-" + str(ySize) + "-" + str(startX) + "-" + str(startY) + ".png")
 
 
-bugday_polygons = read_shp_file("C:\\Users\\oguzh\\PycharmProjects\\graduationProject\\mt_data\\01_Train_Test_Shapefile\\00_Train\\bugday.shp")
 
-rasterVH = rasterio.open("C:\\Users\\oguzh\\PycharmProjects\\graduationProject\\mt_data\\VH.tif")
-rasterVV = rasterio.open("C:\\Users\\oguzh\\PycharmProjects\\graduationProject\\mt_data\\VV.tif")
-rasterVVVH = rasterio.open("C:\\Users\\oguzh\\PycharmProjects\\graduationProject\\mt_data\\VV-VH.tif")
+bugday_polygons_train = read_shp_file(BUGDAY_SHP_FILE_PATH)
+domates_polygons_train = read_shp_file(DOMATES_SHP_FILE_PATH)
+misir_polygons_train = read_shp_file(MISIR_SHP_FILE_PATH)
+misir2_polygons_train = read_shp_file(MISIR2_SHP_FILE_PATH)
+pamuk_polygons_train = read_shp_file(PAMUK_SHP_FILE_PATH)
+uzum_polygons_train = read_shp_file(UZUM_SHP_FILE_PATH)
+yonca_polygons_train = read_shp_file(YONCA_SHP_FILE_PATH)
+zeytin_polygons_train = read_shp_file(ZEYTIN_SHP_FILE_PATH)
+
+polygon_dict_train = {"bugday" : bugday_polygons_train,
+                "domates" : domates_polygons_train,
+                "misir" : misir_polygons_train,
+                "misir2" : misir2_polygons_train,
+                "pamuk" : pamuk_polygons_train,
+                "uzum" : uzum_polygons_train,
+                "yonca" : yonca_polygons_train,
+                "zeytin" : zeytin_polygons_train}
+
+
+rasterRGB = rasterio.open(COMPOSITE_RGB_TIF_FILE_PATH)
 
 xSize = 101
 ySize = 101
-startX = 400
-startY = 1700
-
+startX = 300
+startY = 1500
+'''
 ct=0
 for i in range(300, 700, 200):
-    for j in range(1500, 1900, 200):
-        cutPatchAndCreateMask(rasterVH, 101, 101, i, j, bugday_polygons)
+   for j in range(1500, 1900, 200):
+        cutPatchAndCreateMask(rasterRGB, 101, 101, i, j, polygon_dict_train)
         print(ct)
         ct+=1
-
-#cutPatchAndCreateMask(rasterVH, xSize, ySize, startX, startY, bugday_polygons)
+'''
+cutPatchAndCreateMask(rasterRGB, xSize, ySize, startX, startY, polygon_dict_train)
